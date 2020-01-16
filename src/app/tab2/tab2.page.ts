875c74ba789a6f3ce0a594424bcee05b6e9dd089
Mardi Vester Tamma <mardi@dominopos.com>
2020-01-16 16:28:49 +0800

    initial commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,33 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public photoService: PhotoService, public actionSheetController: ActionSheetController) {}
+
+  ngOnInit() {
+      this.photoService.loadSaved();
+  }
+
+  public async showActionSheet(photo, position) {
+      const actionSheet = await this.actionSheetController.create({
+          header: 'Photos',
+          buttons: [
+            {
+                text: 'Delete',
+                role: 'destructive',
+                icon: 'trash',
+                handler: () => {
+                    this.photoService.deletePicture(photo, position);
+                }
+            }, 
+            {
+                text: 'Cancel',
+                icon: 'close',
+                role: 'cancel',
+                handler: () => {}
+            }
+          ]
+      });
+      await actionSheet.present();
+  }
 
 }
